37c408a0f4538b1340eac1dd839d68e3a02f9fe3
pierriko <pierrick.koch@gmail.com>
2011-06-08 18:34:38 +0700

    rosjava add openjdk

--- morse-ros.sh
+++ morse-ros.sh
@@ -10,20 +10,10 @@ WORKING_DIR=`pwd`
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu lucid main" > /etc/apt/sources.list.d/ros-latest.list'
 wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
 sudo apt-get update
-if [ ! -d /opt/ros/diamondback ]; then
-  echo "#!/bin/sh" > $WORKING_DIR/setup.sh
-  echo "source /opt/ros/diamondback/ros/setup.bash" >> $WORKING_DIR/setup.sh
-fi
-sudo apt-get install build-essential g++ cmake python-setuptools wget subversion git-core mercurial python3.1-dev python-yaml libyaml-dev ruby rubygems doxygen ros-diamondback-desktop-full ant 
+sudo apt-get install build-essential g++ cmake python-setuptools wget subversion git-core mercurial python3.1-dev python-yaml libyaml-dev ruby rubygems doxygen ros-diamondback-desktop-full openjdk-6-jdk ant 
 
-## Morse http://www.openrobots.org/morse/doc/stable/user/installation.html
+# Morse http://www.openrobots.org/morse/doc/stable/user/installation.html
 cd $WORKING_DIR
-#
-#sudo apt-get install python3.2-dev # http://packages.ubuntu.com/natty/python3.2-dev
-#wget http://download.blender.org/release/Blender2.57/blender-2.57b-linux-glibc27-i686.tar.bz2
-#tar jxf blender-2.57b-linux-glibc27-i686.tar.bz2
-#echo "export MORSE_BLENDER=$WORKING_DIR/blender-2.57b-linux-glibc27-i686/blender" >> $WORKING_DIR/setup.sh
-#
 BLENDER=blender-2.56a-beta-linux-glibc27-i686
 if [ ! -d $BLENDER ]; then
   if [ ! -f $BLENDER.tar.bz2 ]; then
@@ -68,13 +58,12 @@ echo "( http://www.openrobots.org/morse/doc/stable/user/installation.html#ros )"
 cd $WORKING_DIR
 if [ ! -d ros-py3 ]; then
   echo "source $WORKING_DIR/ros-py3/setup.bash" >> $WORKING_DIR/setup.sh
-  echo "export JAVA_HOME=/usr/lib/jvm/java-6-openjdk" >> $WORKING_DIR/setup.sh
 fi
 
 sudo easy_install -U rosinstall
 rosinstall $WORKING_DIR/ros-py3 /opt/ros/diamondback http://ias.cs.tum.edu/~kargm/ros_py3.rosinstall http://rosjava.googlecode.com/hg/rosjava.rosinstall
-rosmake ros &&  rosmake ros_comm && rosmake common_msgs
 . $WORKING_DIR/setup.sh
+rosmake ros &&  rosmake ros_comm && rosmake common_msgs
 
 echo "Android SDK : still needed for http://rosjava.googlecode.com "
 echo "( http://developer.android.com/sdk )"
@@ -93,6 +82,7 @@ if [ ! -d android-sdk-linux_x86 ]; then
   done
   echo "(*) please edit target=android-10"
   find $WORKING_DIR/ros-py3/rosjava/android/ -name "default.properties" | xargs gedit &
+  echo "export JAVA_HOME=/usr/lib/jvm/java-6-openjdk" >> $WORKING_DIR/setup.sh
 fi
 
 echo "(*) once you installed the SDK through the android tool, execute:"
